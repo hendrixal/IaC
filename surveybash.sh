3911c16ab3709c3bfa4cc086986bf7e8185ee2ef
Andrew Hendrix <andrew_hendrix@hms.harvard.edu>
2022-10-13 17:41:06 -0400

    adding week 2 bash script

---
+++ surveybash.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#Variables
+answer_no=no
+answer_yes=yes
+
+# Hello thank you for visiting our resturant. Do you have time to complete a quick survey? Please enter "Yes" or "No".
+
+echo "Hello do you have time to complete a survey? Please enter yes or no"
+
+#input from command line from the user
+
+read answer
+
+# if statments based on the above input
+
+if [ "$answer" == "$answer_yes" ]; 
+  then  
+      echo "Please rate your experience 1-5, with 5 being great and 1 being poor"
+  elif [ "$answer" == "$answer_no" ];
+    then
+      echo "No worries, have a great day!"
+      exit 
+  else [ "$answer" != "$answer_yes" ] || [ "$answer" != "$answer_no" ]; 
+      echo "Please type 'yes' or 'no' as a response."
+      exit 
+fi
+
+
+# reading input for rating score
+read rating
+
+# conditional if statement for input
+if [ "$rating" -le 5 ] && [ "$rating" -ge 1 ];
+  then
+      echo "Thank you for completing the survey!"
+  elif [ "$rating" -lt 1 ] || [ "$rating" -gt 5 ] 
+    then
+      echo "This rating is not between 1-5"
+      exit
+fi
+
